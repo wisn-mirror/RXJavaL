c5cdece6eec5dee39e3205706b8798fd2e082a62
Wisn <wuyishun_kmk@outlook.com>
2018-01-18 13:48:14 +0800

    last commit

--- app/src/main/java/com/wisn/rxjava/BootReceiver.java
+++ app/src/main/java/com/wisn/rxjava/BootReceiver.java
@@ -1,8 +1,48 @@
 package com.wisn.rxjava;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 /**
  * Created by wisn on 2017/12/14.
  */
 
-public class Receiver {
+public class BootReceiver extends BroadcastReceiver {
+    private static final String TAG ="BootReceiver" ;
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        Log.e(TAG,"开机了");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true){
+                    SystemClock.sleep(500);
+                    Log.d(TAG,"imdi  time "+new Date(System.currentTimeMillis()));
+                    try {
+                        TelephonyManager telephonyManager = (TelephonyManager) context.getApplicationContext().getSystemService(Context.TELEPHONY_SERVICE);
+                        String deviceId1 = telephonyManager.getDeviceId();
+                        String deviceId= deviceId1.trim();
+                        Pattern p = Pattern.compile("IMEI", Pattern.CASE_INSENSITIVE);
+                        Matcher m = p.matcher(deviceId);
+                        deviceId = m.replaceAll(" ");
+                        deviceId = deviceId.replace(":", " ");
+                        deviceId = deviceId.trim();
+                        Log.d(TAG,"imdi "+deviceId+"  time "+new Date(System.currentTimeMillis()));
+                    } catch (PatternSyntaxException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
 }
--- app/src/main/java/com/wisn/rxjava/MainActivity.java
+++ app/src/main/java/com/wisn/rxjava/MainActivity.java
@@ -1,13 +1,106 @@
 package com.wisn.rxjava;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = "MainActivity";
+
+    private ScrollView mMScroll_info;
+    private TextView mTextResult;
+    private TextView mShareText;
+    private TextView mShareTextdelete;
+    private Button test1;
+    private Button test2;
+    private Button test3;
+    private Button test4;
+    private CheckBox checkbox1;
+    private CheckBox checkbox2;
+    private EditText mEdittext;
+    private Observable<String> mObservable;
+    private Observer mObserver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
+
+    private void initData() {
+        mObservable = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> e) throws Exception {
+                e.onNext("hello rxjava");
+            }
+        });
+        mObserver = new Observer<String>(){
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.e(TAG, "Build.VERSION.SDK_INT：" + Build.VERSION.SDK_INT);
+        if (view == test1) {
+            Log.e(TAG, "Build.VERSION.SDK_INT：" + Build.VERSION.SDK_INT);
+            mObservable.subscribe(mObserver);
+        } else if (view == test2) {
+        } else if (view == test3) {
+        } else if (view == test4) {
+        }
+    }
+
+    private void initView() {
+        mMScroll_info = (ScrollView) findViewById(R.id.scroll_info);
+        mTextResult = (TextView) findViewById(R.id.testResult);
+        mShareText = (TextView) findViewById(R.id.shareText);
+        mShareTextdelete = (TextView) findViewById(R.id.shareTextdelete);
+        mEdittext = (EditText) findViewById(R.id.edittext);
+        test1 = (Button) findViewById(R.id.test1);
+        test2 = (Button) findViewById(R.id.test2);
+        test3 = (Button) findViewById(R.id.test3);
+        test4 = (Button) findViewById(R.id.test4);
+        checkbox1 = (CheckBox) findViewById(R.id.checkbox1);
+        checkbox2 = (CheckBox) findViewById(R.id.checkbox2);
+        test1.setOnClickListener(this);
+        test2.setOnClickListener(this);
+        test3.setOnClickListener(this);
+        test4.setOnClickListener(this);
+        mShareTextdelete.setOnClickListener(this);
+        mShareText.setOnClickListener(this);
     }
 }
